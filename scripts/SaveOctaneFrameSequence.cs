b3cb40f16358637db0afc56144e3a1d8a0869d6a
jalley <jantonio.iglesias@gmail.com>
2017-09-29 13:51:25 +0200

    Created a list of passes tha tautomatically manage output folder, enable/disable and output format. Removed external script to capture semantic segmentation using a second camera script and Render to Target approach. Best way nowadays seems to be to reuse RENDER_PASS_RENDER_LAYER_ID to encode classes.

--- scripts/SaveOctaneFrameSequence.cs
+++ scripts/SaveOctaneFrameSequence.cs
@@ -1,23 +1,49 @@
-﻿// Authors: Fran Molero fmolero@cvc.uab.es
-//          Jose A. Iglesias-Guitian jalley@cvc.uab.es
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////
+// Authors: Fran Molero                fmolero@cvc.uab.es
+//          Marc Puig                  mgarcia@cvc.uab.es
+//          Jose A. Iglesias-Guitian    jalley@cvc.uab.es
+//
 // Date: September 2017
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-/* For now this script under development saves the output of both Octane Renderer and Unity Editor (just for debugging purposes)
-   Main idea: Unity waits until Octane Renderer is ready to dump a rendered frame, meanwhile Unity simulations are frozen using 
-   Time.timeScale = 0; Observed behavior: when increasing m_CaptureFramerate through UI, it can be observed that Octane output 
-   is smoothed.
+/* 
+ * For now this script under development saves the output of the beauty pass of Octane Renderer and some infor passes  (e.g. depth, semantic segmentation).
+ * Main idea: Unity waits until Octane Renderer is ready to dump a rendered frame, meanwhile Unity simulations are frozen using Time.timeScale = 0; 
 */
-// TO DO: The desired behavior would be to ensure the time duration of a certain captured sequence and ideally chosing how 
-// many Octane frames per second we want.
+
+[RequireComponent(typeof(Camera))]
+
+public class RenderPass
+{
+    public Octane.RenderPassId _renderPass;
+    public Octane.ImageSaveType _renderPassOutputType;
+    public bool _capturePass;
+    public string _outputDir;
+
+    public RenderPass(Octane.RenderPassId renderPass, Octane.ImageSaveType renderPassOutputType, bool capturePass = true, string outputDir = "/../Output/Octane")
+    {
+        _renderPass = renderPass;
+        _renderPassOutputType = renderPassOutputType;
+        _capturePass = capturePass;
+        _outputDir = Application.dataPath + outputDir;
+    }
+}
 
 public class SaveOctaneFrameSequence : MonoBehaviour
 {
-    public Octane.RenderPassId[] renderPass;
-    public Octane.ImageSaveType[] renderPassOutputType;
+    //private VZSemanticSegmentation semantic;
+
+    private Octane.RenderPassId[] renderPass;
+    //public Octane.ImageSaveType[] renderPassOutputType;
+    //public bool[] capturePass;
+
+    public List<RenderPass> my_render_passes;
+    public RenderPass[] my_render_passes_array;
 
     //public tOctaneCaptureLayers m_OctaneCaptureLayers;
 
@@ -25,11 +51,13 @@ public class SaveOctaneFrameSequence : MonoBehaviour
     public int m_VSynchCount = 0;
     public uint m_MaxSamplesPerPixel = 0; // this will automatically take later its value from Octane Kernel configuration
     public bool m_CaptureOctane = true;
-    public bool m_CaptureUnity = false;
+    //public bool m_CaptureUnity = false;
+    //public bool m_CaptureSemantic = false;
 
-    private string mOctaneOutputDir;
-    private string mUnityOutputDir;
-    private bool   mCapturedFrame;
+    //private string mOctaneOutputDir;
+    //private string mUnityOutputDir;
+    //private string mSemanticOutputDir;
+    private bool mCapturedFrame;
     private uint m_OctaneSavedFrames = 0;
 
     private uint mPreviousFrameOctaneSpp = 0;
@@ -38,10 +66,12 @@ public class SaveOctaneFrameSequence : MonoBehaviour
     void Start()
     {
         // Create output directories
-        mOctaneOutputDir = Application.dataPath + "/../Output/Octane";
-        Directory.CreateDirectory(mOctaneOutputDir);
-        mUnityOutputDir = Application.dataPath + "/../Output/Unity";        
-        Directory.CreateDirectory(mUnityOutputDir);
+        //mOctaneOutputDir = Application.dataPath + "/../Output/Octane";
+        //Directory.CreateDirectory(mOctaneOutputDir);
+       // mUnityOutputDir = Application.dataPath + "/../Output/Unity";
+        //Directory.CreateDirectory(mUnityOutputDir);
+        //mSemanticOutputDir = Application.dataPath + "/../Output/SemanticSegmentation";
+        //Directory.CreateDirectory(mSemanticOutputDir);
 
         m_MaxSamplesPerPixel = Octane.Renderer.GetLatestRenderStatistics().maxSamplesPerPixel;
         // Configure framerate specifications
@@ -52,13 +82,51 @@ public class SaveOctaneFrameSequence : MonoBehaviour
         mPreviousFrameOctaneSpp = 0;
         m_OctaneSavedFrames = 0;
         mCapturedFrame = false;
+
+        my_render_passes = new List<RenderPass>();
+        my_render_passes.Add(new RenderPass(Octane.RenderPassId.RENDER_PASS_BEAUTY, 
+                                            Octane.ImageSaveType.IMAGE_SAVE_TYPE_EXR, 
+                                            true, 
+                                            "/../Output/Octane/Beauty"));
+        my_render_passes.Add(new RenderPass(Octane.RenderPassId.RENDER_PASS_Z_DEPTH, 
+                                            Octane.ImageSaveType.IMAGE_SAVE_TYPE_EXR, 
+                                            true, 
+                                            "/../Output/Octane/Depth"));
+        my_render_passes.Add(new RenderPass(Octane.RenderPassId.RENDER_PASS_RENDER_LAYER_ID, 
+                                            Octane.ImageSaveType.IMAGE_SAVE_TYPE_PNG8, 
+                                            true, 
+                                            "/../Output/Octane/SemanticSegmentation"));
+        // Add new render passes here ...
+
         //renderPass.Initialize();
-        renderPass = new Octane.RenderPassId[2];
-        renderPassOutputType = new Octane.ImageSaveType[2];
-        renderPass[0] = Octane.RenderPassId.RENDER_PASS_BEAUTY;
-        renderPass[1] = Octane.RenderPassId.RENDER_PASS_Z_DEPTH;
-        renderPassOutputType[0] = Octane.ImageSaveType.IMAGE_SAVE_TYPE_PNG8;
-        renderPassOutputType[1] = Octane.ImageSaveType.IMAGE_SAVE_TYPE_PNG16;
+        //renderPass = new Octane.RenderPassId[Constants.RenderPasses];
+        //renderPassOutputType = new Octane.ImageSaveType[Constants.RenderPasses];
+        //capturePass = new bool[Constants.RenderPasses];
+        //renderPass[0] = Octane.RenderPassId.RENDER_PASS_BEAUTY;
+        //renderPass[1] = Octane.RenderPassId.RENDER_PASS_Z_DEPTH;
+        //renderPass[2] = Octane.RenderPassId.RENDER_PASS_RENDER_LAYER_ID;
+        //renderPassOutputType[0] = Octane.ImageSaveType.IMAGE_SAVE_TYPE_EXR;
+        //renderPassOutputType[1] = Octane.ImageSaveType.IMAGE_SAVE_TYPE_EXR;
+        //renderPassOutputType[2] = Octane.ImageSaveType.IMAGE_SAVE_TYPE_PNG8;
+        //Octane.Renderer.SetRenderPasses(renderPass);
+        //capturePass[0] = true;
+        //capturePass[1] = true;
+        //capturePass[2] = true;
+
+        // list to array
+        my_render_passes_array = my_render_passes.ToArray();
+        int NumPasses = my_render_passes_array.GetLength(0);
+        renderPass = new Octane.RenderPassId[NumPasses];
+        //renderPassOutputType = new Octane.ImageSaveType[NumPasses];
+        //capturePass = new bool[NumPasses];
+
+        for (uint i = 0; i < NumPasses; ++i)
+        {
+            renderPass[i] = my_render_passes_array[i]._renderPass;
+            //renderPassOutputType[i] = my_render_passes_array[i]._renderPassOutputType;
+            //capturePass[i] = my_render_passes_array[i]._capturePass;
+            Directory.CreateDirectory(my_render_passes_array[i]._outputDir);
+        }        
         Octane.Renderer.SetRenderPasses(renderPass);
     }
 
@@ -81,22 +149,23 @@ public class SaveOctaneFrameSequence : MonoBehaviour
             {
                 m_OctaneSavedFrames++;
                 //string frame_str = "octane_fr_" + m_OctaneSavedFrames/*+ "_realfr_" + Time.frameCount.ToString()*/;
-                for (int pass = 0; pass < renderPass.GetLength(0); pass++)
+                for (int pass = 0; (pass < my_render_passes_array.GetLength(0)) && my_render_passes_array[pass]._capturePass; pass++)
                 {
-                    string frame_str = string.Format("{0}/octane_fr{1:D04}_pass{2}_spp{3}.png", mOctaneOutputDir, m_OctaneSavedFrames, pass, Octane.Renderer.SampleCount);
+                    string frame_str = string.Format("{0}/octane_fr{1:D04}_pass{2}_spp{3}.png", my_render_passes_array[pass]._outputDir, m_OctaneSavedFrames, pass, Octane.Renderer.SampleCount);
                     Octane.Renderer.SaveImage(/*Octane.RenderPassId.RENDER_PASS_BEAUTY*/renderPass[pass],
                                           frame_str,
-                                          /*Octane.ImageSaveType.IMAGE_SAVE_TYPE_PNG8*/ renderPassOutputType[1],
+                                          /*Octane.ImageSaveType.IMAGE_SAVE_TYPE_PNG8*/ my_render_passes_array[pass]._renderPassOutputType,
                                           true/*asynchronous*/);
                 }
-                //Octane.Renderer.SaveImage(Octane.RenderPassId.RENDER_PASS_AMBIENT_OCCLUSION,
-                //                          mOctaneOutputDir + "/" + frame_str + "_AO_" + Octane.Renderer.SampleCount + "spp",
-                //                          Octane.ImageSaveType.IMAGE_SAVE_TYPE_PNG16,
-                //                          false/*synchronous*/);
-                //Octane.Renderer.SaveImage(Octane.RenderPassId.RENDER_PASS_Z_DEPTH,
-                //                          mOctaneOutputDir + "/" + frame_str + "_depth_" + Octane.Renderer.SampleCount + "spp",
-                //                          Octane.ImageSaveType.IMAGE_SAVE_TYPE_PNG16,
-                //                          false/*synchronous*/);
+                // save semantic segmentation (double check it exists)
+                //if (m_CaptureSemantic)
+                //{
+                    //GameObject semantic_obj = GameObject.Find("GTCamera");
+                    //Camera semantic_cam = semantic_obj.GetComponent<Camera>();
+                    //semantic = semantic_obj.GetComponent<VZSemanticSegmentation>();
+                    //semantic.SaveCameraToDisk(semantic_cam, mSemanticOutputDir);
+                //}
+
                 print("Writing Octane image " + m_OctaneSavedFrames + " rendered with " + Octane.Renderer.GetLatestRenderStatistics().samplesPerPixel + "spp.");
                 Time.captureFramerate = m_CaptureFramerate;
                 Time.timeScale = 1;
@@ -110,14 +179,14 @@ public class SaveOctaneFrameSequence : MonoBehaviour
             mPreviousFrameOctaneSpp = Octane.Renderer.SampleCount;
         }
 
-        if (m_CaptureUnity)
-        {
+        //if (m_CaptureUnity)
+        //{
             // Capture Unity Render
             // To do: Append filename to folder name (format is '0005 shot.png"')
             //string name = string.Format("{0}/{1:D04} shot.png", folder, Time.frameCount);
-            string name_str = "unity_fr_" + Time.frameCount.ToString();
+            //string name_str = "unity_fr_" + Time.frameCount.ToString();
             // Capture the screenshot to the specified file.
-            ScreenCapture.CaptureScreenshot(mUnityOutputDir + "/" + name_str + ".png");
-        }
+            //ScreenCapture.CaptureScreenshot(mUnityOutputDir + "/" + name_str + ".png");
+        //}
     }
 }
